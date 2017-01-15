0d07cd9f5acb4dd793a51b7c6d2aa120db9f8950
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-15 23:29:49 +0300

    (chore) use main-container directive on the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './containers/main';
 
 @NgModule({
   // Specify a list of directives to use
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
