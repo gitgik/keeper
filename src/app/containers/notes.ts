9fa317909744857e15f2c56c44e2a4bc1b49d0ce
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-17 23:02:35 +0300

    (feature) define note creation  ui component

--- src/app/containers/notes.ts
+++ src/app/containers/notes.ts
@@ -2,20 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'notes-container',
+  styles: [`
+    .notes: {
+      padding-top: 50px;
+      margin-top:10px;
+    }
+
+    .creator {
+      margin-bottom: 40px;
+    }
+  `],
   template: `
     <div class="row center-xs notes">
-      <div class="col-xs-6 creator">Your notes</div>
+      <div class="col-xs-6 creator">
+        <note-creator></note-creator>
+      </div>
       <div class="notes col-xs-8">
         <div class="row between-xs">
           <note-card
             class="col-xs-4"
             [note]="note"
             *ngFor="let note of notes; let i = index"
-            (checked)="onNoteChecked(i)"
+            (checkEvent)="onNoteChecked(i)"
           >
           </note-card>
         </div>
       </div>
+    </div>
   `
 })
 
