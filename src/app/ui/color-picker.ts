7be829d63be4fa9bdabe47c57ed4a5cef3388c6b
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-22 19:28:57 +0300

    (chore) only show color picker when the icon for picking is clicked

--- src/app/ui/color-picker.ts
+++ src/app/ui/color-picker.ts
@@ -40,24 +40,33 @@ import {
   `],
   template: `
     <div class="color-selector">
-      <i class="material-icons icon">color_lens</i>
-      <div class="selector row center-xs">
+      <i class="material-icons icon"
+      (click)="showColorPicker(true)">color_lens</i>
+      <div class="selector row center-xs" *ngIf="isSelectorVisible">
         <div class="color"
         *ngFor="let color of colors"
-        (click)="selectColor(color)"></div>
+        (click)="selectColor(color)"
+        [ngStyle]="{'background-color': color}"></div>
       </div>
     </div>
   `
 })
 
 export class ColorPicker {
-  // Css colors to populate color picker
+  // Expect an input of colors to populate color picker
   @Input() colors: string[] = [];
   // Event to be fired once a color is selected
   @Output() selected = new EventEmitter();
+  isSelectorVisible: boolean = false;
 
   // Selects the background color for the current note
   selectColor(color: string) {
     this.selected.next(color);
+    // hide the color picker
+    this.showColorPicker(false);
+  }
+
+  showColorPicker(value: boolean) {
+    this.isSelectorVisible = value;
   }
 }
