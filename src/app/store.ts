62077505446f11383eb959cf8e359d6ed849c65e
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-01-30 16:59:58 +0300

    (feature) define a store to store the app state

--- src/app/store.ts
+++ src/app/store.ts
@@ -14,7 +14,7 @@ export interface Note {
 }
 
 export interface State {
-  notes: Note()
+  notes: Note[]
 }
 
 
@@ -22,9 +22,28 @@ const defaultState: State = {
   notes: []
 }
 
+/* Create a Behavior subject.
+We use it because it's a subject that must always return a value on subscription
+even if it hasn't received a next()
+*/
 const _store = new BehaviorSubject<State>(defaultState);
 
 @Injectable()
 export class Store {
+  private store = _store;
+  changes = this.store.asObservable().distinctUntilChanged()
+
+  setState(state: State) {
+    this.store.next(state)
+  }
+
+  getState(): State {
+    return this.store.value;
+  }
+
+  purge() {
+    // For things like logging out
+    this.store.next(defaultState);
+  }
 
 }
