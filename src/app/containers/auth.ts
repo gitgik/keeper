15f763fcc60ef2ed8c3bde40e102901361d72a03
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-04 23:28:48 +0300

    (feature) define auth boilerplate

---
+++ src/app/containers/auth.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import 'rxjs/Rx';
+
+@Injectable()
+export class AuthService implements CanActivate {
+
+}
