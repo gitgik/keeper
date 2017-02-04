2db51a5a2648d8b740d8b9f1c50146a4ab4af2c1
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-04 23:45:39 +0300

    (feature) add auth service to services folder

--- src/app/containers/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import 'rxjs/Rx';
-
-@Injectable()
-export class AuthService implements CanActivate {
-
-}
---
+++ src/app/services/auth.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import 'rxjs/Rx';
+
+@Injectable()
+export class AuthService implements CanActivate {
+
+  JWT_KEY: string  = 'keeper_token';
+  JWT: string = '';
+
+  constructor(private router: Router) {}
+
+  isAuthorized(): boolean {
+    return Boolean(this.JWT);
+  }
+
+  canActivate(): boolean {
+    const canActivate = this.isAuthorized();
+    this.onCanActivate(canActivate);
+    return canActivate;
+  }
+
+  // Redirect to auth if user can not authenticate
+  onCanActivate(canActivate: boolean) {
+    if (!canActivate) {
+      this.router.navigate(['','auth']);
+    }
+  }
+}
