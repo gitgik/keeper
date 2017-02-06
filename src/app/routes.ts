a31c58944aedee2feba77e5b9687159e02bc42d7
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-06 17:20:04 +0300

    (feature) integrate the auth service in routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,19 +2,20 @@ import { RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { MainContainer } from './containers/main';
 import { NotesContainer } from './containers/notes';
-import {AuthService} from './services/auth';
+import { AuthService } from './services/auth';
+
+import { AuthContainer } from './containers/auth';
 
 // Create a module with all the router providers and directives
 export const routes: ModuleWithProviders = RouterModule.forRoot([
   {
     path: '',
     component: MainContainer,
-    canActivate: [AuthService],
+    canActivate: [ AuthService ],
     children: [
       { path: '', component: NotesContainer }
     ]
   },
-  {
-    path: '**', redirectTo: ''
-  }
+  { path: 'auth', component: AuthContainer },
+  { path: '**', redirectTo: ''}
 ])
