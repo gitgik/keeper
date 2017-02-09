38b860be2a66317c15c40a9ee170e0911c6e8d9b
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-02-09 15:05:57 +0300

    (feature) add store, store helpers and api to auth

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { StoreHelper } from './store-helper';
+import { Store } from '../store';
+import { ApiService } from './api';
 import 'rxjs/Rx';
+
 
 @Injectable()
 export class AuthService implements CanActivate {
 
   JWT_KEY: string  = 'keeper_token';
-  JWT: string = 'p';
+  JWT: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private store: Store,
+    private storeHelper: StoreHelper,
+  ) {
+    const token = window.localStorage.getItem(this.JWT)
+
+  }
 
   isAuthorized(): boolean {
     return Boolean(this.JWT);
